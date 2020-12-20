ba5e7c798d0a551e0275ac27f28da1a86b425794
AshriyaSinha <73244852+AshriyaSinha@users.noreply.github.com>
2020-12-20 18:06:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	Ball=loadImage("Images/paper.png");
+	Boxes=loadImage("Images/dustbin green.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	papp = new Paper(250,500,10); 	
+
+	ground = new Ground(400,650,800,25);
+
+	box1=new Box(580,640,100,20, {restitution:0, isStatic:false});
+	box2=new Box(555,630,20,95, {restitution:0, isStatic:false});
+	box3=new Box(654,630,20,95, {restitution:0, isStatic:false});
+
+
+	Engine.run(engine);
+  
+}
+function preload(){
+	ball=loadImage("Images/paper.png");
+	boxes=loadImage("Images/dustbin green.png");
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+  box1.display();
+  box2.display();
+  box3.display();
+  ground.display();
+  papp.display();
+}
+
+function keyPressed (){
+	if (keyCode == UP_ARROW ){
+		Matter.Body.applyForce (papp.body,papp.body.position,
+			{
+			x:50,
+			y:-50,
+		});
+	}
+}
+
+
